e3915ed276c75d1c5f8bb7447165131b7059cb62
Andrew Heald <andrew@heald.co.uk>
2010-12-07 17:12:08 +0000

    CHM-32: Add authentication.

--- src/main/java/uk/org/sappho/jira4j/soap/GetParentService.java
+++ src/main/java/uk/org/sappho/jira4j/soap/GetParentService.java
@@ -12,15 +12,18 @@ import uk.org.sappho.jira.rpc.soap.client.GetParentServiceLocator;
 public class GetParentService {
 
     private final GetParent service;
+    private final String token;
 
-    public GetParentService(String url) throws MalformedURLException, ServiceException {
+    public GetParentService(String url, String username, String password) throws MalformedURLException,
+            ServiceException, RemoteException {
 
         service = new GetParentServiceLocator().getSapphoGetparentV1(new URL(url
                 + "/rpc/soap/sappho-getparent-v1"));
+        token = service.login(username, password);
     }
 
     public String getParent(String issueKey) throws RemoteException {
 
-        return service.getParentKey(issueKey);
+        return service.getParentKey(token, issueKey);
     }
 }
